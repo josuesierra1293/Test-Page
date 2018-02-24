823961f8e5748934298cd6413ef8a4aa24b8003e
josue <josue.sierra1293@gmail.com>
2018-02-24 13:52:49 -0700

    add new features carousel

--- js/function.js
+++ js/function.js
@@ -1,19 +1,19 @@
 var count = 0
 $('.carousel.carousel-slider').carousel({fullWidth: true});
 
-// function cuenta(){
-// 	if(count == 5){
-//     $('.carousel').carousel('next', 1); // Move next n times
-//     count = 0
-// }else{
-// 	count ++
-// }
-// }
+function cuenta(){
+	if(count == 5){
+    $('.carousel').carousel('next', 1); // Move next n times
+    count = 0
+}else{
+	count ++
+}
+}
 
-// function ini(){
-// 	cuenta();
-// 	setInterval("cuenta()",1000);
-// }
+function ini(){
+	cuenta();
+	setInterval("cuenta()",1000);
+}
 
 
 // function add(type) {
